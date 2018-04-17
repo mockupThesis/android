03fc97964e0d126c450f367e7082f2f6efa7384b
Chrome <belfegor219@gmail.com>
2018-04-17 16:12:42 +0200

    MainDrawer skeleton

--- app/src/main/java/hu/webandmore/androidmocapclient/app/ui/main/MainActivity.java
+++ app/src/main/java/hu/webandmore/androidmocapclient/app/ui/main/MainActivity.java
@@ -1,6 +1,10 @@
 package hu.webandmore.androidmocapclient.app.ui.main;
 
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -8,9 +12,8 @@ import android.view.MenuItem;
 
 import hu.webandmore.androidmocapclient.R;
 
-public class MainActivity extends AppCompatActivity implements MainScreen {
-
-    private static String TAG = "MainActivity";
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,17 +22,36 @@ public class MainActivity extends AppCompatActivity implements MainScreen {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
+
         if (id == R.id.action_settings) {
             return true;
         }
@@ -37,23 +59,21 @@ public class MainActivity extends AppCompatActivity implements MainScreen {
         return super.onOptionsItemSelected(item);
     }
 
+    @SuppressWarnings("StatementWithEmptyBody")
     @Override
-    public void checkMockupConnection() {
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
 
-    }
+        if (id == R.id.nav_home) {
+            
+        } else if (id == R.id.nav_settings) {
 
-    @Override
-    public void showMockupConnectionError() {
+        } else if (id == R.id.nav_debug) {
 
-    }
+        }
 
-    @Override
-    public void checkMqttConnection() {
-
-    }
-
-    @Override
-    public void showMqttConnectionError() {
-
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
